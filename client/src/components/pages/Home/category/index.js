dbbb66169882ba60c147f8a02f0f8558cd7797ad
raven-wills <raven.wills@gmail.com>
2018-07-19 19:11:20 -0500

    Category images fixed

--- client/src/components/pages/Home/category/index.js
+++ client/src/components/pages/Home/category/index.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import ScrollingNav from "../../../ScrollingNav";
+import "../Home.css";
 
 export default class Category extends Component {
+  // componentDidMount() {}
+
   render() {
     const { name, children } = this.props.location.state;
     return (
       <div>
         <ScrollingNav />
         <h1>{name}</h1>
-        <ul>{children.map(subcategory => <li>{subcategory.name}</li>)}</ul>
+        <div className="container">
+          {children.map(subcategory => (
+            <div className="item">{subcategory.name}</div>
+          ))}
+        </div>
       </div>
     );
   }
